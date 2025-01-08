cbe84fa1d854e3e6f91ca58f718208d6e39b6824
Caio <caiocesarpoke2@hotmail.com>
2025-01-08 10:42:05 -0300

    retirado filter

--- src/App.vue
+++ src/App.vue
@@ -9,48 +9,46 @@
   })
 
   function getNumber1(evento) {
-  estado.num1 = evento.target.value;
-  if (estado.num1 == '')  {
-    estado.num1 = 0;
-  }
-  estado.num1 = parseFloat(estado.num1)
-  getOperacao();
-}
+    estado.num1 = evento.target.value;
+    if (estado.num1 == '')  {
+      estado.num1 = 0;
+    }
+    estado.num1 = parseFloat(estado.num1)
+    getOperacao();
+  };
 
-function getNumber2(evento) {
-  estado.num2 = evento.target.value;
-  if (estado.num2 == '')  {
-    estado.num2 = 0;
-  }
-  estado.num2 = parseFloat(estado.num2)
-  getOperacao();
-}
+  function getNumber2(evento) {
+    estado.num2 = evento.target.value;
+    if (estado.num2 == '')  {
+      estado.num2 = 0;
+    }
+    estado.num2 = parseFloat(estado.num2)
+    getOperacao();
+  };
 
-function getOperador(evento) {
-  estado.operador = evento.target.value;
-  getOperacao();
-}
+  function getOperador(evento) {
+    estado.operador = evento.target.value;
+    getOperacao();
+  };
 
   const soma = () => {
     return estado.resultado = (estado.num1 + estado.num2);
-  }
+  };
 
   const subtrai = () => {
     return estado.resultado = (estado.num1 - estado.num2);
-  }
+  };
 
   const dividi = () => {
     return estado.resultado = (estado.num1 / estado.num2);
-  }
+  };
 
   const multiplica = () => {
     return estado.resultado = (estado.num1 * estado.num2);
-  }
+  };
 
   const getOperacao = () => {
-    const {filtro} = estado;
-
-    switch (filtro) {
+    switch (estado.operador) {
       case 'soma':
         return soma();
       case 'subtrai':
@@ -60,17 +58,17 @@ function getOperador(evento) {
       case 'multiplica':
         return multiplica();
     }
-  }
+  };
 </script>
 
 <template>
   <div class="container text-center">
     <h1 class="p-3">Calculadora aritmética</h1>
 
-    <input class="me-3" type="number" id="num1" placeholder="Primeiro número" @keyup="getNumber1()">
-    <input class="me-3" type="number" id="num2" placeholder="Segundo número" @keyup="getNumber2()">
+    <input class="me-3" type="number" id="num1" placeholder="Primeiro número" @keyup="getNumber1">
+    <input class="me-3" type="number" id="num2" placeholder="Segundo número" @keyup="getNumber2">
     <label class="me-3" for="operacao">Escolha a operação</label>
-    <select id="operacao" @change="getOperador()">
+    <select id="operacao" @change="getOperador">
       <option selected value="soma">Soma</option>
       <option value="subtrai">Subtração</option>
       <option value="multiplica">Multiplicação</option>
